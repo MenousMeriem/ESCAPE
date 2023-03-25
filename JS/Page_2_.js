88342973869dc252d6d069cdae6bcbca28f6bcd6
MenousMeriem <meriemmenous1@gmail.com>
2023-03-25 13:36:51 +0100

    2 commit

---
+++ JS/Page_2_.js
@@ -0,0 +1,6 @@
+document
+.querySelector(".hamburger__container")
+.addEventListener("click",(e)=> {
+    document.querySelector(".hamburger__content").classList.toggle("active")
+    document.querySelector(".hamburger__container").classList.toggle("active")
+})
